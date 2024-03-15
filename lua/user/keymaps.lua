d93a37d76a2e6cb9ec3ee03dcc2e8beb218d7044
its-me-ojas <crestfallenwizard@gmail.com>
2024-03-16 03:58:21 +0530

    keymap updated

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -4,4 +4,4 @@ lvim.keys.insert_mode["<C-j>"] = "<Down>"
 lvim.keys.insert_mode["<C-k>"] = "<Up>"
 lvim.keys.insert_mode["<C-l>"] = "<Right>"
 
-vim.api.nvim_set_keymap('n', '<leader>tt', ':TroubleToggle', { noremap = true, silent = true })
+lvim.keys.normal_mode["<leader>tt"] = ":TroubleToggle<CR>"
