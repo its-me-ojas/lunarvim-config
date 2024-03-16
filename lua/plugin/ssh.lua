13d8ff946203fad70e004b4d5c2f1f09791d0025
its-me-ojas <crestfallenwizard@gmail.com>
2024-03-17 01:11:11 +0530

    added ssh plugin

---
+++ lua/plugin/ssh.lua
@@ -0,0 +1,7 @@
+require('remote-sshfs').setup({})
+require('telescope').load_extension 'remote-sshfs'
+
+return {
+  'nosduco/remote-sshfs.nvim',
+  dependencies = { 'nvim-telescope/telescope.nvim' } -- optional if you declare plugin somewhere else
+}
